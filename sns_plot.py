454f050eebcec93c6fb51f278c8135347e3e64a8
Gary Hutson <44023992+StatsGary@users.noreply.github.com>
2020-12-14 14:33:10 +0000

    Updated sns_plot.py file
    Updated sns_plot.py file and added in relevant context.

--- sns_plot.py
+++ sns_plot.py
@@ -1,11 +1,17 @@
+###########################NHS R Community - Reticulate and R - a happy union ###################
+#################################CODE WRITTEN BY GARY HUTSON ####################################
+
 #!/usr/bin/env python
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+import warnings as warn
 from pandas import Series, DataFrame
 from matplotlib import rcParams
 
+
 rcParams.update({'figure.autolayout': True}) #Set the matplot lib to auto layout
+
 '''
 Read in the data frame from the previous example
 
@@ -19,14 +25,18 @@ print(df)
 
 def make_sns_plot(data, hue_val):
   if hue_val == "":
+    print("There was no hue value entered.")
     exit
   else:
     sns.pairplot(data=data, hue=hue_val)
 
 
 # Do some descriptive stats
+with warn.catch_warnings():
+  warn.simplefilter('ignore')
+  make_sns_plot(df, 'TTBS_mins') #Suppress the zero variance errors here
 
-make_sns_plot(df, 'TTBS_mins')
+# Write out descriptive statistics
 df.describe()
 df.ndim
 df.dtypes
